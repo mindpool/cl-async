b4769641d583bd4485eea0ec3daf98979657c556
Duncan McGreggor <oubiwann@mindpool.io>
2012-10-17 19:57:26 -0700

    Fixed up long lines.

--- signal.lisp
+++ signal.lisp
@@ -46,7 +46,10 @@
            (ev (getf sig-data :ev))
            (original-lisp-signal-handler (getf sig-data :original-handler)))
       (le:event-del ev)
-      (cffi:foreign-funcall "signal" :int signo :pointer original-lisp-signal-handler :pointer)
+      (cffi:foreign-funcall "signal"
+                            :int signo
+                            :pointer original-lisp-signal-handler
+                            :pointer)
       (free-pointer-data signo-sym)
       (free-pointer-data data-pointer))
     (setf *signal-handlers* (remove signo *signal-handlers*))))
@@ -68,9 +71,11 @@
   "Replace the current handler for the signal number under signo, and return a
    pointer to the handler that is being replaced."
   (save-callbacks (signal-sym signo) fn)
-  (cffi:foreign-funcall "signal" :int signo :pointer (cffi:callback lisp-signal-cb) :pointer))
+  (cffi:foreign-funcall "signal" :int signo :pointer
+    (cffi:callback lisp-signal-cb) :pointer))
 
-(cffi:defcallback signal-cb :void ((signo :int) (event :short) (data-pointer :pointer))
+(cffi:defcallback signal-cb :void 
+  ((signo :int) (event :short) (data-pointer :pointer))
   "All signals come through here."
   (let* ((callbacks (get-callbacks data-pointer))
          (signal-cb (getf callbacks :signal-cb))
@@ -83,19 +88,24 @@
 (defun signal-handler (signo signal-cb &key event-cb)
   "Setup a one-time signal handler for the given signo. This also sets up a
    lisp signal handler, so if a signal comes through while lisp is running
-   instead of the event loop, it will run the same callback. All signal handlers
-   are restored on event loop exit."
+   instead of the event loop, it will run the same callback. All signal
+   handlers are restored on event loop exit."
   (check-event-loop-running)
   ;; un-bind this signal handler if it is already bound. this ensures we don't
   ;; lose the original lisp signal handler when we overwrite it.
   (free-signal-handler signo)
   (let* ((data-pointer (create-data-pointer))
-         (ev (le:event-new *event-base* signo (logior le:+ev-signal+ le:+ev-persist+) (cffi:callback signal-cb) data-pointer))
-         (lisp-signal-handler (set-lisp-signal-handler signo (lambda () (le:event-active ev))))
+         (ev (le:event-new *event-base* signo 
+           (logior le:+ev-signal+ le:+ev-persist+)
+           (cffi:callback signal-cb) data-pointer))
+         (lisp-signal-handler
+           (set-lisp-signal-handler signo (lambda () (le:event-active ev))))
          (signo-sym (signal-sym signo)))
     (le:event-add ev (cffi:null-pointer))
-    (save-callbacks data-pointer (list :signal-cb signal-cb :event-cb event-cb))
-    (attach-data-to-pointer data-pointer (list :ev ev :original-handler lisp-signal-handler))
+    (save-callbacks data-pointer 
+      (list :signal-cb signal-cb :event-cb event-cb))
+    (attach-data-to-pointer data-pointer
+      (list :ev ev :original-handler lisp-signal-handler))
     ;; make sure we can find the event/original handler from just the signo
     (attach-data-to-pointer signo-sym data-pointer)
     ;; add this signal to the list of active signals
